3a1fdb45b1ce9bdbad80a3266d39d731bb9c4be6
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-01-05 13:05:02 -0600

    simplify author

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -116,11 +116,6 @@ export const Authors = defineDocumentType(() => ({
   contentType: 'mdx',
   fields: {
     name: { type: 'string', required: true },
-    avatar: { type: 'string' },
-    occupation: { type: 'string' },
-    company: { type: 'string' },
-    email: { type: 'string' },
-    twitter: { type: 'string' },
     linkedin: { type: 'string' },
     github: { type: 'string' },
     layout: { type: 'string' },
