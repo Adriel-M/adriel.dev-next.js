ded94b6f5d060a52a49b934d3fd26361f1455e0d
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-01-05 12:07:19 -0600

    no newsletter

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -33,9 +33,9 @@ const siteMetadata = {
     // },
   },
   newsletter: {
-    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
-    // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    // // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
+    // // Please add your .env file and modify it according to your selection
+    // provider: 'buttondown',
   },
   // comments: {
   //   // If you want to use an analytics provider you have to add it to the
