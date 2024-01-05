6411ba03907b203ac29e18e75df8b2955be36e01
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-01-05 03:28:18 -0600

    delete ThemeSwitcher code

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,6 @@ import headerNavLinks from '@/data/headerNavLinks'
 import Logo from '@/data/logo.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
-import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
 
 const Header = () => {
