51678f76f59002bddb6f12298abbfb8cae282271
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-01-06 06:18:22 -0600

    what if in node_modules

--- scripts/vercel-build.sh
+++ scripts/vercel-build.sh
@@ -2,13 +2,13 @@
 
 echo "Starting the script..."
 
-# Check if .next/install-state.gz exists and copy it to .yarn/install-state.gz
-if [ -f ".next/install-state.gz" ]; then
-    echo "Copying .next/install-state.gz to .yarn/install-state.gz..."
-    cp .next/install-state.gz .yarn/install-state.gz
+# Check if node_modules/install-state.gz exists and copy it to .yarn/install-state.gz
+if [ -f "node_modules/install-state.gz" ]; then
+    echo "Copying node_modules/install-state.gz to .yarn/install-state.gz..."
+    cp node_modules/install-state.gz .yarn/install-state.gz
     echo "Copy complete."
 else
-    echo ".next/install-state.gz does not exist. Skipping copy."
+    echo "node_modules/install-state.gz does not exist. Skipping copy."
 fi
 
 # Set an environment variable YARN_CACHE_FOLDER=./.next/cache/yarn
@@ -21,10 +21,10 @@ echo "Running 'yarn install --immutable'..."
 yarn install --immutable
 echo "'yarn install --immutable' complete."
 
-# Check if .yarn/install-state.gz exists and copy it to .next/install-state.gz
+# Check if .yarn/install-state.gz exists and copy it to node_modules/install-state.gz
 if [ -f ".yarn/install-state.gz" ]; then
-    echo "Copying .yarn/install-state.gz back to .next/install-state.gz..."
-    cp .yarn/install-state.gz .next/install-state.gz
+    echo "Copying .yarn/install-state.gz back to node_modules/install-state.gz..."
+    cp .yarn/install-state.gz node_modules/install-state.gz
     echo "Copy complete."
 else
     echo ".yarn/install-state.gz does not exist. Skipping copy."
