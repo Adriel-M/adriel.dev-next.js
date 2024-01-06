d24ef1b0683325d9a367b3a05439dd173bb6b665
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-01-06 05:50:23 -0600

    Access-Control-Allow-Origin

--- next.config.js
+++ next.config.js
@@ -27,6 +27,10 @@ const securityHeaders = [
     key: 'Referrer-Policy',
     value: 'strict-origin-when-cross-origin',
   },
+  {
+    key: 'Access-Control-Allow-Origin',
+    value: 'https://identity.tinajs.io',
+  },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
   {
     key: 'X-Frame-Options',
