b5f80d62f0d606ba74b141f5f8efafcf6c730e1c
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-02-18 13:33:09 -0700

    Clean up next.config.js

--- next.config.js
+++ next.config.js
@@ -4,30 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-// You might need to insert additional domains in script-src if you are using external services
-const ContentSecurityPolicy = `
-  default-src 'self'
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is localhost:*;
-  style-src 'self' 'unsafe-inline';
-  img-src * blob: data:;
-  media-src *.s3.amazonaws.com;
-  connect-src *;
-  font-src 'self' data:;
-  frame-src giscus.app
-`
-
 const securityHeaders = [
-  // // // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-  // {
-  //   key: 'Content-Security-Policy',
-  //   value: ContentSecurityPolicy.replace(/\n/g, ''),
-  // },
-  // // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  // {
-  //   key: 'Referrer-Policy',
-  //   value: 'strict-origin-when-cross-origin',
-  // },
-  // // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
   {
     key: 'X-Frame-Options',
     value: 'DENY',
@@ -60,7 +37,6 @@ const securityHeaders = [
 module.exports = () => {
   const plugins = [withContentlayer, withBundleAnalyzer]
   return plugins.reduce((acc, next) => next(acc), {
-    reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     eslint: {
       dirs: ['app', 'components', 'layouts', 'scripts'],
@@ -93,7 +69,7 @@ module.exports = () => {
         },
       ]
     },
-    webpack: (config, options) => {
+    webpack: (config) => {
       config.module.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
