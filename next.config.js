9de5772ba0325f099906332b8df449ad262f92cd
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-03-06 23:31:20 -0500

    remove vercelscript, add jsdelivr to script-src csp

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' va.vercel-scripts.com localhost:*;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' localhost:* cdn.jsdelivr.net;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   connect-src *;
