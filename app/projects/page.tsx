fde354881c52c515e3f6f6fec8f090b8e1556cb5
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-03-09 20:19:14 -0500

    use emoji

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Some projects I worked on
+            Some projects I worked on 🧑‍💻
           </p>
         </div>
         <div className="container py-12">
