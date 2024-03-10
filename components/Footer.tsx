3ac97793e19d60db6d94c1984b60a2b346c3a390
Adriel Martinez <20115135+Adriel-M@users.noreply.github.com>
2024-03-10 00:40:14 -0500

    remove dark css (#83)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,31 +15,28 @@ export default function Footer() {
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
           <SocialIcon kind="rss" href="/rss.xml" size={6} />
         </div>
-        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+        <div className="mb-2 flex space-x-2 text-sm text-gray-500">
           <div>
             Logo from{' '}
-            <Link
-              className="hover:text-primary-500 dark:hover:text-primary-400"
-              href="https://github.com/twitter/twemoji"
-            >
+            <Link className="hover:text-primary-500" href="https://github.com/twitter/twemoji">
               Twemoji
             </Link>
           </div>
           <div>{` • `}</div>
           <Link
-            className="hover:text-primary-500 dark:hover:text-primary-400"
+            className="hover:text-primary-500"
             href="https://creativecommons.org/licenses/by/4.0/"
           >
             CC BY 4.0
           </Link>
         </div>
-        <div className="mb-8 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+        <div className="mb-8 flex space-x-2 text-sm text-gray-500">
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
           <div>Adriel Martinez</div>
           <div>{` • `}</div>
           <Link
-            className="hover:text-primary-500 dark:hover:text-primary-400"
+            className="hover:text-primary-500"
             href="https://creativecommons.org/licenses/by/4.0/"
           >
             CC BY 4.0
--- layouts/PostSimple.tsx
+++ layouts/PostSimple.tsx
@@ -25,11 +25,11 @@ export default function PostLayout({ content, next, prev, children }: LayoutProp
       <article>
         <div>
           <header>
-            <div className="space-y-1 border-b border-gray-200 pb-10 text-center dark:border-gray-700">
+            <div className="space-y-1 border-b border-gray-200 pb-10 text-center">
               <dl>
                 <div>
                   <dt className="sr-only">Published on</dt>
-                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                  <dd className="text-base font-medium leading-6 text-gray-500">
                     <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
                   </dd>
                 </div>
@@ -39,16 +39,14 @@ export default function PostLayout({ content, next, prev, children }: LayoutProp
               </div>
             </div>
           </header>
-          <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:divide-y-0">
-            <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-              <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
+          <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 xl:divide-y-0">
+            <div className="divide-y divide-gray-200 xl:col-span-3 xl:row-span-2 xl:pb-0">
+              <div className="prose max-w-none pb-8 pt-10">{children}</div>
             </div>
             <footer>
               {tags && tags.length > 0 && (
                 <div className="py-4 xl:py-8">
-                  <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
-                    Tags
-                  </h2>
+                  <h2 className="text-xs uppercase tracking-wide text-gray-500">Tags</h2>
                   <div className="flex flex-wrap">
                     {tags.sort().map((tag) => (
                       <Tag key={tag} text={tag} />
@@ -61,7 +59,7 @@ export default function PostLayout({ content, next, prev, children }: LayoutProp
                   {prev && prev.path && (
                     <Link
                       href={`/${prev.path}`}
-                      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                      className="text-primary-500 hover:text-primary-600"
                       aria-label={`Previous post: ${prev.title}`}
                     >
                       &larr; {prev.title}
@@ -72,7 +70,7 @@ export default function PostLayout({ content, next, prev, children }: LayoutProp
                   {next && next.path && (
                     <Link
                       href={`/${next.path}`}
-                      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                      className="text-primary-500 hover:text-primary-600"
                       aria-label={`Next post: ${next.title}`}
                     >
                       {next.title} &rarr;
