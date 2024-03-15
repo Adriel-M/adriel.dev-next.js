dc7db4c354458eccbb6e0d71976327a5dd7e7ecc
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-03-15 18:52:17 -0400

    what about same file?

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,5 @@
 import NextImage, { ImageProps } from 'next/image'
-import test from '../public/static/images/cockroach-multiregion-demo/client.png'
+import test from './main.png'
 
 const Image = ({ ...rest }: ImageProps) => {
   return <NextImage {...rest} src={test} />
