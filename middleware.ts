31d88a0c837c25c649caa93f6bdee415c094278e
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-03-19 22:05:01 -0400

    middleware 301 when image found, else 404

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,16 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import images from '@/core/Images'
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const newImagePath = images[request.nextUrl.pathname]
 
   if (!newImagePath) {
-    return NextResponse.redirect(new URL('/', request.url))
+    return NextResponse.next()
   }
 
   const url = request.nextUrl.clone()
   url.pathname = newImagePath
-  return NextResponse.redirect(url)
+  return NextResponse.redirect(url, 301)
 }
 
 // See "Matching Paths" below to learn more
