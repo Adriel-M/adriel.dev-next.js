21e575939c7b6e9005020040ad4e1866413ee0e8
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-03-19 20:12:01 -0400

    extract generating image map into separate file

---
+++ core/Images.ts
@@ -0,0 +1,32 @@
+import RequireContext = __WebpackModuleApi.RequireContext
+const importFromFolder = (
+  context: RequireContext
+): { [key: string]: { default: { src: string } } } => {
+  const images = {}
+
+  for (const key of context.keys()) {
+    // keys are duplicated in a sense that there are two keys for the same image
+    // one with a relative path inside images folder and the other with images
+    // folder a prefix
+    if (key.startsWith('images')) continue
+
+    const newKey = key.replace('./', '/images/')
+    images[newKey] = context(key)
+  }
+
+  return images
+}
+
+const getImages = () => {
+  console.log('getImages')
+  const keyToModule = importFromFolder(require.context('../images', true, /\.(png|jpe?g|svg)$/))
+  const keyToPath: { [key: string]: string } = {}
+
+  for (const [key, value] of Object.entries(keyToModule)) {
+    keyToPath[key] = value.default.src
+  }
+
+  return keyToPath
+}
+
+export default getImages()
