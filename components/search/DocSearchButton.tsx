ee204521c8d2c71e5f0cf938c4155127973ac7eb
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-03-26 19:20:38 -0400

    Add transformer + Hit component

--- components/search/DocSearchButton.tsx
+++ components/search/DocSearchButton.tsx
@@ -5,14 +5,50 @@ import '@docsearch/css/dist/modal.css'
 import './variables.css'
 
 import { DocSearch } from '@docsearch/react'
+import {
+  DocSearchHit,
+  InternalDocSearchHit,
+  StoredDocSearchHit,
+} from '@docsearch/react/dist/esm/types'
 import { AlgoliaConfig } from 'pliny/search/Algolia'
 
+import CustomLink from '@/components/Link'
 import siteMetadata from '@/data/siteMetadata'
+
+const transformItems = (items: DocSearchHit[]): DocSearchHit[] => {
+  return items.map((item) => {
+    const url = new URL(item.url)
+    return {
+      ...item,
+      url: url.pathname + url.hash,
+    }
+  })
+}
+
+const Hit = ({
+  hit,
+  children,
+}: {
+  hit: InternalDocSearchHit | StoredDocSearchHit
+  children: React.ReactNode
+}) => {
+  return <CustomLink href={hit.url}>{children}</CustomLink>
+}
 
 const DocSearchButton = () => {
   const searchConfig = siteMetadata.search as AlgoliaConfig
   const { appId, apiKey, indexName } = searchConfig.algoliaConfig
-  return <DocSearch appId={appId} apiKey={apiKey} indexName={indexName} />
+  return (
+    <DocSearch
+      appId={appId}
+      apiKey={apiKey}
+      indexName={indexName}
+      placeholder="Search"
+      transformItems={transformItems}
+      hitComponent={Hit}
+      disableUserPersonalization={true}
+    />
+  )
 }
 
 export default DocSearchButton
