370e033ab7b1724727e2de06b3019ae3c564d391
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-03-27 01:30:36 -0400

    Remove trailing ellipsis from algolia. Add via css instead.

--- components/search/DocSearchButton.tsx
+++ components/search/DocSearchButton.tsx
@@ -15,8 +15,23 @@ import { AlgoliaConfig } from 'pliny/search/Algolia'
 import CustomLink from '@/components/Link'
 import siteMetadata from '@/data/siteMetadata'
 
+const TRAILING_ELLIPSIS_PATTERN = /\s+…$/
 const transformItems = (items: DocSearchHit[]): DocSearchHit[] => {
   return items.map((item) => {
+    // Strip the ellipsis at the end. Add ellipsis via css instead.
+    const snippetValue = item._snippetResult?.content?.value
+    if (snippetValue) {
+      item._snippetResult.content.value = snippetValue.replace(TRAILING_ELLIPSIS_PATTERN, '')
+    }
+    if (item._snippetResult.hierarchy) {
+      for (const value of Object.values(item._snippetResult.hierarchy)) {
+        const hierarchyValue = value.value
+        if (hierarchyValue && typeof hierarchyValue === 'string') {
+          value.value = hierarchyValue.replace(TRAILING_ELLIPSIS_PATTERN, '')
+        }
+      }
+    }
+
     const url = new URL(item.url)
     return {
       ...item,
