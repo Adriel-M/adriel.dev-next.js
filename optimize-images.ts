16137c7e4cbce27c949d197a0d04a2133a27c6f1
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-04-03 21:02:30 -0400

    100% quality

--- optimize-images.ts
+++ optimize-images.ts
@@ -7,7 +7,7 @@ const optimizeImages = async () => {
     cwd: process.cwd(),
     pattern: `${mediaPath}/*.{jpg,jpeg,png}`,
     sizes: [160, 320, 480, 640, 960, 1280, 2560, 3840],
-    quality: 75,
+    quality: 100,
   })
 }
 
