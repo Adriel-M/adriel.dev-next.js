8bb164e5f4eaa2d1faf5bebe2575a2ef8622046b
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-04-04 21:12:14 -0400

    CSP changes

--- next.config.js
+++ next.config.js
@@ -7,11 +7,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' localhost:* cdn.jsdelivr.net;
-  style-src 'self' 'unsafe-inline' cdn.jsdelivr.net;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' localhost:* static.cloudflareinsights.com;
+  style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   connect-src *;
-  font-src 'self' data: cdn.jsdelivr.net;
+  font-src 'self';
 `
 
 const securityHeaders = [
