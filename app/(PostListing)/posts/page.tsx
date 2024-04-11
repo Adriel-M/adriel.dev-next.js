905de291c007c5a514f9d6fcce4636c9789b447d
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-04-10 23:29:52 -0400

    Sort posts in Velite prepare step.

--- app/(PostListing)/posts/page.tsx
+++ app/(PostListing)/posts/page.tsx
@@ -1,12 +1,11 @@
 import PostListingPage from '@/app/(PostListing)/PostListingPage'
 import { genPageMetadata } from '@/app/seo'
 import { getAllPosts } from '@/lib/CollectionUtils'
-import { sortPosts } from '@/lib/PlinyUtils'
 
 export const metadata = genPageMetadata({ title: 'Posts' })
 
 export default function Page() {
-  const posts = sortPosts(getAllPosts())
+  const posts = getAllPosts()
 
   return <PostListingPage posts={posts} pageNumber={1} />
 }
