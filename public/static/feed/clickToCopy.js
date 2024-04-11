65345c732a9629360ba392979c8c4f31e35b9f48
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-04-10 22:21:28 -0400

    prettier clickToCopy js code

--- public/static/feed/clickToCopy.js
+++ public/static/feed/clickToCopy.js
@@ -1,10 +1,13 @@
 function clickToCopy(buttonId, copyFromId) {
-  document.getElementById(buttonId).addEventListener('click', function() {
+  document.getElementById(buttonId).addEventListener('click', function () {
     const textToCopy = document.getElementById(copyFromId).innerText
-    navigator.clipboard.writeText(textToCopy).then(function() {
-      // empty
-    }, function(err) {
+    navigator.clipboard.writeText(textToCopy).then(
+      function () {
+        // empty
+      },
+      function (err) {
         alert('Could not copy text, please copy it manually')
-    })
+      }
+    )
   })
 }
