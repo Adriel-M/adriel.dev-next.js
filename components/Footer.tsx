935c7a8f78c00eb74c143a264afd5f56337732e8
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-04-19 01:59:22 -0400

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,25 +7,10 @@ export default function Footer() {
   return (
     <footer>
       <div className="mt-16 flex flex-col items-center">
-        <div className="mb-3 flex space-x-4">
+        <div className="mb-4 flex space-x-4">
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
           <SocialIcon kind="rss" href="/rss.xml" size={6} />
-        </div>
-        <div className="mb-2 flex space-x-2 text-sm text-gray-500">
-          <div>
-            Logo from{' '}
-            <Link className="hover:text-primary-500" href="https://github.com/twitter/twemoji">
-              Twemoji
-            </Link>
-          </div>
-          <div>{` • `}</div>
-          <Link
-            className="hover:text-primary-500"
-            href="https://creativecommons.org/licenses/by/4.0/"
-          >
-            CC BY 4.0
-          </Link>
         </div>
         <div className="mb-8 flex space-x-2 text-sm text-gray-500">
           <div>{`© ${new Date().getFullYear()}`}</div>
