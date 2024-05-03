efd1a1bbee90a63a4910d9ed34b5aee02de688d4
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-05-03 00:22:16 -0400

    set active header

--- app/posts/[slug]/FloatingTOC.tsx
+++ app/posts/[slug]/FloatingTOC.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
 import Link from '@/components/Link'
 
 interface TocEntry {
@@ -20,20 +24,34 @@ interface Props {
   toc: TocEntry[]
 }
 
-const TocEntryList = ({ isNested, level }: { isNested?: boolean; level?: TocEntry[] }) => {
+const TocEntryList = ({
+  activeSectionUrl,
+  isNested,
+  level,
+}: {
+  activeSectionUrl: string
+  isNested?: boolean
+  level?: TocEntry[]
+}) => {
   if (!level?.length) return
 
   const borderCss = isNested ? 'pl-2 border-l-2 border-gray-200/25' : ''
 
   return (
     <ul className={borderCss}>
-      {level.map((entry) => {
+      {level.map((entry, index) => {
+        // (if no active [say from page load], set the first as active
+        // else set the active when matching the section url
+        const isActive =
+          (!activeSectionUrl && !isNested && index === 0) || entry.url === activeSectionUrl
+
+        const activeCss = isActive ? 'text-primary-500' : ''
         return (
           <li className="list-inside list-disc" key={entry.url}>
-            <Link className="no-underline hover:text-primary-500" href={entry.url}>
+            <Link className={'no-underline hover:text-primary-600 ' + activeCss} href={entry.url}>
               {entry.title}
             </Link>
-            <TocEntryList isNested={true} level={entry.items} />
+            <TocEntryList activeSectionUrl={activeSectionUrl} isNested={true} level={entry.items} />
           </li>
         )
       })}
@@ -42,10 +60,31 @@ const TocEntryList = ({ isNested, level }: { isNested?: boolean; level?: TocEntr
 }
 
 const FloatingTOC = ({ toc }: Props) => {
+  const [activeId, setActiveId] = useState('')
+
+  const handleScroll = () => {
+    const headers = document.querySelectorAll('.content-header')
+    for (const header of headers) {
+      if (header.id === 'footnote-label') continue
+
+      const rect = header.getBoundingClientRect()
+
+      if (rect.top <= 10) {
+        setActiveId(`#${header.id}`)
+      }
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
   return (
     <div className="sticky top-20 hidden h-0 px-8 text-xs w-toc-2xl lg:block xl:w-toc-4xl">
       <div className="pb-2 text-sm underline underline-offset-4">Table of Contents</div>
-      <TocEntryList level={toc} />
+      <TocEntryList activeSectionUrl={activeId} level={toc} />
     </div>
   )
 }
