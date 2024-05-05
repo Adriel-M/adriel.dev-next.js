0c0384a5f7547bb4b81fd6e9f28e8ac2303d794d
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-05-04 21:23:39 -0400

    Add way to update date

--- scripts/cli/updatePosts.ts
+++ scripts/cli/updatePosts.ts
@@ -5,6 +5,10 @@ import inquirer from 'inquirer'
 import { postsPath } from './paths'
 
 const EXIT = 'exitPrompt'
+
+const DATE_FIELD = 'date'
+const LASTMOD_FIELD = 'lastmod'
+
 export const updatePost = async () => {
   const postsFolder = `${process.cwd()}/${postsPath}`
 
@@ -25,13 +29,32 @@ export const updatePost = async () => {
     process.exit()
   }
 
+  const field = (
+    await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'field',
+        message: 'Which field do you want to update',
+        choices: [DATE_FIELD, LASTMOD_FIELD, new inquirer.Separator(), EXIT],
+      },
+    ])
+  ).field
+
+  if (field === EXIT) {
+    process.exit()
+  }
+
   const filePath = `${postsFolder}/${fileName}`
   const file = Bun.file(filePath)
   const fileContent = await file.text()
 
   const { content, data } = matter(fileContent)
 
-  data.lastmod = new Date()
+  if (field === DATE_FIELD) {
+    data.date = new Date()
+  } else if (field === LASTMOD_FIELD) {
+    data.lastmod = new Date()
+  }
 
   await Bun.write(filePath, matter.stringify(content, data))
 
