e755f8102d449a614ae4d06537d550826cf8eca3
Adriel <20115135+Adriel-M@users.noreply.github.com>
2024-05-09 01:22:22 -0400

    disallow indexing feed + og

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: ['/og', '*rss.xml$', '*atom.xml$'],
     },
     sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
     host: siteMetadata.siteUrl,
