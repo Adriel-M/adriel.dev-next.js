86ca6889dbb1c7d010e6088be97d2fcdcdbdd193
Adriel <20115135+Adriel-M@users.noreply.github.com>
2025-04-16 19:29:43 -0400

    Add force-static for robots

--- app/robots.ts
+++ app/robots.ts
@@ -2,6 +2,8 @@ import { MetadataRoute } from 'next'
 
 import siteMetadata from '@/lib/siteMetadata'
 
+export const dynamic = 'force-static'
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
