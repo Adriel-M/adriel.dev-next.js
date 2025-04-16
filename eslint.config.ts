7405612724d1896eb9a9547e19a30f76a15dfd77
Adriel <20115135+Adriel-M@users.noreply.github.com>
2025-04-16 01:12:25 -0400

    remove eslint js

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,7 +2,6 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 import { FlatCompat } from '@eslint/eslintrc'
-import js from '@eslint/js'
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 import { defineConfig } from 'eslint/config'
@@ -16,8 +15,6 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
 })
 
 export default defineConfig([
