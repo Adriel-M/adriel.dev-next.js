8dc6a3e77cac69b6acc9adb3bb42e6a81bceee58
Adriel <20115135+Adriel-M@users.noreply.github.com>
2025-04-23 14:07:11 -0400

    clean up Image component

--- components/Image.tsx
+++ components/Image.tsx
@@ -3,13 +3,13 @@ import NextImage, { ImageProps } from 'next/image'
 import images from '@/lib/Images'
 import siteConfig from '@/lib/siteConfig'
 
-const Image = ({ ...rest }: ImageProps) => {
-  let imageSrc = rest.src
+const Image = ({ src, ...rest }: ImageProps) => {
+  let imageSrc = src
+  // serve the bundled image since we're not serving from the public folder
   if (
     typeof imageSrc === 'string' &&
     imageSrc.startsWith(`/${siteConfig.bundledImagesFolderName}`)
   ) {
-    // do a static import since we're not putting images in the public folder
     imageSrc = images[imageSrc]
   }
   return <NextImage {...rest} src={imageSrc} />
