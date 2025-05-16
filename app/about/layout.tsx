87bc2fe64f8d3c43bd121cc62d24c320ec03ed48
Adriel Martinez <20115135+Adriel-M@users.noreply.github.com>
2025-05-16 14:14:00 -0500

    Only use SectionContainer in top most layout, make padding px-6 so it looks better on mobile (#403)

--- app/about/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react'
-
-import SectionContainer from '@/components/SectionContainer'
-
-export default function AboutLayout({ children }: { children: ReactNode }) {
-  return <SectionContainer>{children}</SectionContainer>
-}
--- app/posts/[slug]/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react'
-
-import SectionContainer from '@/components/SectionContainer'
-
-export default function PostLayout({ children }: { children: ReactNode }) {
-  return <SectionContainer>{children}</SectionContainer>
-}
