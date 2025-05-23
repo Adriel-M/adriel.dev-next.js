2508af5bc075771c958bdb677fed739e12098482
Adriel <20115135+Adriel-M@users.noreply.github.com>
2025-05-23 01:26:32 -0500

    Revert "Use ts version of next.config (#416)"
    This reverts commit 1f1c2e990d339109abc12cc4f1fa3a0e2b22d3d3.

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
-import bundleAnalyzer from '@next/bundle-analyzer'
-import { NextConfig } from 'next'
-// @ts-expect-error: No Types definition
+import { build } from 'velite'
 import withPlugins from 'next-compose-plugins'
 
-const isDev = process.argv.includes('dev')
-const isBuild = process.argv.includes('build')
-if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
-  process.env.VELITE_STARTED = '1'
-  import('velite').then((m) => m.build({ watch: isDev, clean: !isDev }))
-}
+import bundleAnalyzer from '@next/bundle-analyzer'
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
@@ -63,7 +56,10 @@ const securityHeaders = [
   },
 ]
 
-const nextConfig: NextConfig = {
+/**
+ * @type {import('next/dist/next-server/server/config').NextConfig}
+ **/
+const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'mdx'],
   eslint: {
     dirs: ['app', 'components', 'layouts', 'lib', 'scripts', 'content', 'velite-collections'],
@@ -76,11 +72,30 @@ const nextConfig: NextConfig = {
       },
     ]
   },
+  webpack: (config) => {
+    config.plugins.push(new VeliteWebpackPlugin())
+
+    return config
+  },
   images: {
     loader: 'custom',
-    loaderFile: './lib/imageLoader.ts',
+    loaderFile: './lib/imageLoader.ts'
   },
   poweredByHeader: false,
+}
+
+class VeliteWebpackPlugin {
+  static started = false
+  apply(/** @type {import('webpack').Compiler} */ compiler) {
+    // executed three times in nextjs
+    // twice for the server (nodejs / edge runtime) and once for the client
+    compiler.hooks.beforeCompile.tap('VeliteWebpackPlugin', async () => {
+      if (VeliteWebpackPlugin.started) return
+      VeliteWebpackPlugin.started = true
+      const dev = compiler.options.mode === 'development'
+      await build({ watch: dev, clean: !dev })
+    })
+  }
 }
 
 export default withPlugins([withBundleAnalyzer], nextConfig)
